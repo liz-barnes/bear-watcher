efe9195bd8a79442fd3d1848320f0963daf6d502
Liz <liz@Liz-s-Mac.local>
2020-08-23 21:55:21 -0500

    start building out caught & attempt buttons and functions to log activity

--- javascripts/components/river.js
+++ javascripts/components/river.js
@@ -1,17 +1,63 @@
+
 const buildBearCards = (array) => {
     $("#containerBearCards").html('');
     array.forEach((bear) => {
         $('#containerBearCards').append(`
-            <div class="card text-center m-5" style="width: 18rem;">
-                <div class="" id="containerImage">    
-                    <img class="card-img-top" src="${bear.bearImage}">
-                </div>    
-                <div class="card-body">
-                    <h3 class="card-text">${bear.bearName}</h3>
+            <div id="containerBearCard" class="card text-center m-5" style="width: 20rem;">
+                <div id="containerBearInfo">
+                    <div id="bearImage">
+                        <img class="card-img-top" src="${bear.bearImage}">
+                    </div>
+                    <div id="bearName">
+                        <h3 class="card-text">${bear.bearName}</h3>
+                    </div>
+                </div>
+                <div id="containerButtons" class="mt-5">
+                    <button type="button" class="btn btn-primary mr-2" id="btnCaught">Caught Fish!</button>
+                    <button type="button" class="btn btn-primary ml-2" id="btnAttempt">Attempt Fish!</button>
+                </div>
+                <div id="containerJournalLog">
+                    <div id="activity"></div>
+                    <div id="timestamp"></div>
                 </div>
             </div>
         `);
     });
 };
 
+const caughtFish = () => {
+    $('#btnCaught').on('click', () => {
+        let caught = [];
+        const timestamp = new Date();
+        $('#activity').html(`
+            Caught a fish! [Total: 1]
+        `)
+        $('#timestamp').html(`
+        ${timestamp}
+        `)
+    });
+    // const attemptButtonClick = (arr) => {
+    //     arr.forEach((bear, index) => {
+    //         $(`#attempt-${index}`).click(() => {
+    //             let attempts = {};
+    //             attempts.time = new Date();
+    //             attempts.type = 'Attempt';
+    //             $(`#attemptsAndCatches-${index}`).append(
+    //                 `<div class="${attempts.type} m-2">
+    //                     <div>${attempts.type}: ${attempts.time}</div>
+    //                 </div>`
+    //             )
+    //         })
+    //     })
+    };
+
   export { buildBearCards };
+
+//   <div class="card text-center m-5" style="width: 18rem;">
+//                 <div class="" id="containerImage">    
+//                     <img class="card-img-top" src="${bear.bearImage}">
+//                 </div>    
+//                 <div class="card-body">
+//                     <h3 class="card-text">${bear.bearName}</h3>
+//                 </div>
+//             </div>
